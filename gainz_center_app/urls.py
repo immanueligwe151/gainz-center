04d816df3365ce4add142e7ff04f8e558f2864f3
Immanuel <64789685+immanueligwe151@users.noreply.github.com>
2025-01-19 02:49:01 +0000

    added functionality to enable logged in users to view extra pages, coded frontend for the my-profile page

--- gainz_center_app/urls.py
+++ gainz_center_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, faqs, about, our_gym, login, signup, logout, my_profile
+from .views import home, faqs, about, our_gym, login, signup, logout, my_profile, my_prs, my_sessions, personal_trainers
 
 urlpatterns = [
     path('', home, name='home'),
@@ -9,6 +9,10 @@ urlpatterns = [
     path('login', login, name='login'),
     path('signup', signup, name='signup'),
     path('logout', logout, name='logout'),
-    path('my-profile', my_profile, name='my-profile')
+    path('my-profile', my_profile, name='my-profile'),
+    path('my-prs', my_prs, name='my-prs'),
+    path('my-sessions', my_sessions, name='my-sessions'),
+    path('personal-trainers', personal_trainers, name='personal-trainers'),
+    
 
 ]
--- gainz_center_app/views.py
+++ gainz_center_app/views.py
@@ -6,6 +6,7 @@ from django.core.exceptions import ValidationError
 
 # Create your views here.
 def home(request):
+    manually_clear_messages(request)
     return render(request, 'gainz_center_app/home.html')
 
 def faqs(request):
@@ -20,6 +21,8 @@ def our_gym(request):
     return render(request, 'gainz_center_app/our-gym.html', { 'equipments' : equipments })
 
 def login(request):
+    manually_clear_messages(request)
+
     if 'username' in request.session:
         return redirect('my-profile') #redirects logged in user to profile page if they try to cheekily type in the url for the login page
 
@@ -31,7 +34,6 @@ def login(request):
             user = Users.objects.get(username=username)
             if user.check_password(password):
                 request.session['username'] = username  # Store session
-                messages.success(request, "You have succesfully logged in!", extra_tags="login_success")
                 return redirect('my-profile')  #to redirect to the profile page
             else:
                 messages.error(request, "Your username or password is incorrect, please try again.", extra_tags="login_failure")
@@ -104,17 +106,43 @@ def signup(request):
 def logout(request):
     request.session.flush()
     messages.success(request, "Logged out successfully.")
+    manually_clear_messages(request)
     return redirect('home')
 
 def my_profile(request):
+    user = None #declaring it at the head here so it's within scope
+    if 'username' not in request.session:
+        return redirect('home') #go back to home page if user is not logged in
+    else:
+        username = request.session['username'] #retrieves username from session
+        try:
+            user = Users.objects.get(username=username)
+        except Users.DoesNotExist:
+            return redirect('login') #should not be needed since user can't access the page anyway if not logged in
+
+    return render(request, 'gainz_center_app/my-profile.html', { 'user': user })
+
+def my_sessions(request):
     if 'username' not in request.session:
         return redirect('home')
 
-    return render(request, 'gainz_center_app/my-profile.html')
+    return render(request, 'gainz_center_app/my-sessions.html')
+
+def my_prs(request):
+    if 'username' not in request.session:
+        return redirect('home')
+
+    return render(request, 'gainz_center_app/my-prs.html')
+
+def personal_trainers(request):
+    if 'username' not in request.session:
+        return redirect('home')
+
+    return render(request, 'gainz_center_app/personal-trainers.html') 
 
 
 
 
-
-
-    
+def manually_clear_messages(request):
+    if '_messages' in request.session:
+        del request.session['_messages']
