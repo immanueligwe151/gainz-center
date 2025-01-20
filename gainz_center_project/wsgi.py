f65a36b645e33c35822b1b2c5a4a3cd5e4ed7c67
Immanuel <64789685+immanueligwe151@users.noreply.github.com>
2025-01-20 07:36:20 +0000

    Configure static files

--- gainz_center_project/wsgi.py
+++ gainz_center_project/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from whitenoise import WhiteNoise
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gainz_center_project.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
